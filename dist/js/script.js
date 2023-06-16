c02d57d9c276f24e940f20ad6c933f0a2c0eaced
Daria Dorna <perelka4@onet.pl>
2023-06-16 20:27:14 +0200

    Add price calculating 7.6

--- dist/js/script.js
+++ dist/js/script.js
@@ -157,24 +157,22 @@
           // check if there is param with a name of paramId in formData and if it includes optionId
           if (formData[paramId] && formData[paramId].includes(optionId)) {
             // check if the option is not default
-            if (option.default != undefined) {
+            if (!option.default == true) {
               // add option price to price variable
-              return price + option.price;
+              price += option.price;
             }
           } else {
             // check if the option is default
             if (option.default == true) {
               // reduce price variable
-              return price - option.price;
-
+              price -= option.price;
             }
-
-
-
-
           }
         }
+
       }
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
     }
   }
   const app = {
