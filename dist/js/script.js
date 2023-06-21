fd8f3e74940f498261f496ab8b1341c81b92977c
Daria Dorna <perelka4@onet.pl>
2023-06-21 12:45:35 +0200

    Deleting deliveryFee bug

--- dist/js/script.js
+++ dist/js/script.js
@@ -395,6 +395,10 @@
         thisCart.update();
       });
 
+      thisCart.dom.productList.addEventListener('remove', function () {
+        thisCart.remove(event.detail.cartProduct);
+      });
+
     }
 
 
@@ -440,6 +444,18 @@
 
     }
 
+    remove(cartProduct) {
+      const thisCart = this;
+
+      cartProduct.dom.wrapper.remove();
+
+      const indexOfProduct = thisCart.products.indexOf(cartProduct);
+      thisCart.products.splice(indexOfProduct, 1);
+
+      thisCart.update();
+    }
+
+
   }
   class CartProduct {
     constructor(menuProduct, element) {
@@ -456,6 +472,7 @@
 
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
+      thisCartProduct.initActions();
 
 
 
@@ -483,8 +500,33 @@
         thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
       });
     }
-  }
 
+    remove() {
+      const thisCartProduct = this;
+      const event = new CustomEvent('remove', {
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct,
+        },
+      });
+
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+
+    }
+
+    initActions() {
+      const thisCartProduct = this;
+      thisCartProduct.dom.edit.addEventListener('click', function (event) {
+        event.preventDefault();
+      });
+      thisCartProduct.dom.remove.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisCartProduct.remove();
+      });
+
+
+    }
+  }
 
 
 
