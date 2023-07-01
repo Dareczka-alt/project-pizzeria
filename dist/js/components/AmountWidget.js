fa794ee4b9f92b3bbf0c2060ea35672e34b13a43
Daria Dorna <perelka4@onet.pl>
2023-07-01 15:16:44 +0200

    Add Data and Hourpicker files

--- dist/js/components/AmountWidget.js
+++ dist/js/components/AmountWidget.js
@@ -1,5 +1,6 @@
-import { settings, select } from '../settings.js';
+import { select, settings } from '../settings.js';
 import BaseWidget from './BaseWidget.js';
+
 class AmountWidget extends BaseWidget {
   constructor(element) {
     super(element, settings.amountWidget.defaultValue);
@@ -8,46 +9,43 @@ class AmountWidget extends BaseWidget {
     thisWidget.getElements(element);
     thisWidget.setValue(thisWidget.dom.input.value);
     thisWidget.initActions();
-
   }
 
   getElements() {
     const thisWidget = this;
-
 
     thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
     thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
     thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
   }
 
-
   isValid(value) {
     return !isNaN(value)
       && value >= settings.amountWidget.defaultMin
-      && value <= settings.amountWidget.defaultMax
+      && value <= settings.amountWidget.defaultMax;
   }
 
   renderValue() {
     const thisWidget = this;
-
     thisWidget.dom.input.value = thisWidget.value;
   }
 
   initActions() {
     const thisWidget = this;
+
     thisWidget.dom.input.addEventListener('change', function () {
-      thisWidget.setValue(thisWidget.input.value);
+      thisWidget.value = thisWidget.dom.input.value;
     });
+
     thisWidget.dom.linkDecrease.addEventListener('click', function (event) {
       event.preventDefault();
       thisWidget.setValue(thisWidget.value - 1);
-
     });
+
     thisWidget.dom.linkIncrease.addEventListener('click', function (event) {
       event.preventDefault();
       thisWidget.setValue(thisWidget.value + 1);
     });
   }
-
 }
 export default AmountWidget;
